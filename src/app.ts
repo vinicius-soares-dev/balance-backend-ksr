c5bc28f4fca43e258bb1fbffcc1316db9743ed31
vinicius-soares-dev <vinicius.soares.dev>
2024-01-13 18:16:11 -0300

    feat: add 'controllers' folder

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import "express-async-errors";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,6 @@ const routes = express.Router();
 const LoginController = require("../controllers/LoginController/index");
 
 
-
 routes.get("/", LoginController.LoginController);
 
 export default routes;
