45ccdd450b9af22259f959199b960a827e51ab4d
vinicius-soares-dev <vinicius.soares.dev>
2024-01-15 19:16:01 -0300

    feat: add login controller

--- src/controllers/LoginController/index.ts
+++ src/controllers/LoginController/index.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import "express-async-errors";
 
 export const LoginController = (req: Request, res: Response) => {
+  const { email, password } = req.body;
   try {
-    res.status(200).send("Hello world, I'm here");
+    if(email == "vinicius@gmail.com" && password == 12345678) {
+      return res.status(200).json({ message: 'User logged in successfully' })
+    }
   } catch {
-    res.status(500).send("error");
+    return res.status(401).json({ error: 'Email or Password incorrect.'});
   }
 }
