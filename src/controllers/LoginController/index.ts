bc525681f02c3f4e03c8d816d1fea40b0a9e75e5
vinicius-soares-dev <vinicius.soares.dev>
2024-01-19 00:28:59 -0300

    feat: add read data

--- src/controllers/LoginController/index.ts
+++ src/controllers/LoginController/index.ts
@@ -1,13 +1,7 @@
 import { Request, Response } from "express";
 import "express-async-errors";
+// import LoginModel from "../../models/LoginModel";
 
-export const LoginController = (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  try {
-    if(email == "vinicius@gmail.com" && password == 12345678) {
-      return res.status(200).json({ message: 'User logged in successfully' })
-    }
-  } catch {
-    return res.status(401).json({ error: 'Email or Password incorrect.'});
-  }
+export const readLogin = (req: Request, res: Response) => {
+  res.status(200).send("<h1>Success</h1>");
 }
